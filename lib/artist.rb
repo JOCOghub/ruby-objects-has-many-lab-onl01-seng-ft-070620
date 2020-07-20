0734627fcd82bd6374b450b4f111905ca04f453b
Joseph cohen <jojocohen57@gmail.com>
2020-07-20 20:05:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,8 +5,9 @@ class Artist
     @name = name
   end
  
-  def add_song(song)
-    song.artist = self
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
   end
  
   def songs
