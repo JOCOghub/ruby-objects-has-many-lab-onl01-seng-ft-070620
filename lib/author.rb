1f8a0269f21331611890acaa80a3285baafdfeec
Joseph cohen <jojocohen57@gmail.com>
2020-07-20 20:57:35 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,7 +16,7 @@ class Author
     post.author = self
   end
    
-  def add_post_by_name(song_name)
+  def add_post_by_title(song_name)
     post = Post.new(song_name)
     post.author = self
   end
